b3586936b98a77a204210456f633013db985eac0
MorGu246 <morgueta36@gmail.com>
2026-07-19 10:45:43 -0300

    entrance_and_exit

--- App.js
+++ App.js
@@ -12,7 +12,9 @@ import WebView from 'react-native-webview';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import LottieView from 'lottie-react-native';
-import { MyInput } from './LoginPage';
+//import { MyInput } from './LoginPage';
+import MyEntrance from './(tabs)/entrance';
+import MyExit from './(tabs)/exit';
 
 export default function App() {
   return (
@@ -20,62 +22,10 @@ export default function App() {
     //   <Text>Open up App.js to start working on your app!</Text>
     //   <StatusBar style="auto" />
     // </View>
-    <MyInput></MyInput>
+    // <MyInput></MyInput>
+    <MyExit/>
   );
 }
-// export function MyInput() {
-//     let input2 = useRef(null);
-//     let [myName, setMyName] = useState('')
-//     let [myPass, setMyPass] = useState('')
-//     return (
-//         <SafeAreaView style={{ flex: 1 }}>
-//             <TouchableWithoutFeedback  //ככה כל נגיעה במסך תבטל את המקלדת
-//                 onPress={()=>Keyboard.dismiss()}>
-//                 <KeyboardAvoidingView // שהמקלדת לא תסתיר את האינפוט
-//                     style={stylesInput.screen}
-//                     behavior={Platform.OS == 'ios' ? 'padding' : 'height'} // מזהה מערכת הפעלה
-//                     >
-//                     <Text>MyInput</Text>
-//                     <TextInput
-//                     // returnKeyType='search' // מציג איך ייראה הכפתור של השליחה
-//                         placeholder='your name...'
-//                         // keyboardType='numeric'/'email-address' // /מייל מקלדת עם מספרים
-//                         // secureTextEntry={true} //סיסמה
-//                         //   editable={false}
-//                         onSubmitEditing={()=>input2.current.focus()}
-//                         value={myName}
-//                         onChangeText={setMyName}
-//                         style={stylesInput.input} />
-//                     <TextInput
-//                         ref={input2}
-//                     // returnKeyType='search' // מציג איך ייראה הכפתור של השליחה
-//                         placeholder='your password...'
-//                         // keyboardType='numeric'/'email-address' // /מייל מקלדת עם מספרים
-//                         secureTextEntry={true} //סיסמה
-//                         //   editable={false}
-//                         onSubmitEditing={()=>console.log("hello") //יפעיל את ה-ref בלחיצה - יעבור לאינפוט הבא
-//                         }
-//                         value={myPass}
-//                         onChangeText={setMyPass}
-//                         style={stylesInput.input} />
-//                     {/* <TextInput
-//                         multiline={true}
-//                         style={stylesInput.input} /> */}
-//                 </KeyboardAvoidingView>
-//             </TouchableWithoutFeedback>
-//         </SafeAreaView>
-//     )
-// }
-// const stylesInput = StyleSheet.create({
-//     input: {
-//         width: "90%",
-//         borderWidth: 1
-//     },
-//     screen: {
-//         flex: 1,
-//         justifyContent: "center"
-//     }
-// })
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 const styles = StyleSheet.create({
